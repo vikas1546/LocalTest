608620077d85785c7c7c663815be03bbb3822bc7
vikas sharma <vikas6917@hotmail.com>
2017-09-11 12:46:45 +0530

    Deleted file2,login.csss,login.htm,mytest1.txt etc files

--- src/LocalTest/Startup.cs
+++ src/LocalTest/Startup.cs
@@ -30,6 +30,8 @@ namespace LocalTest
                 app.UseDeveloperExceptionPage();
             }
 
+
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
